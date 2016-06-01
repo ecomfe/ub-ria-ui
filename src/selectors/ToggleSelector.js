0ef12ca0dfde2cff8b6ff8c3e2e8e29ff8bc8031
yanghuabei <yanghuabei@baidu.com>
2016-06-01 15:58:29 +0800

    toggleSelector增加默认title文字，处理bool类型属性初始值

--- src/selectors/ToggleSelector.js
+++ src/selectors/ToggleSelector.js
@@ -38,9 +38,19 @@ define(
                 initOptions: function (options) {
                     var properties = {
                         textField: null,
-                        collapseAfterChange: true
+                        collapseAfterChange: true,
+                        defaultText: '请选择'
                     };
                     options = u.extend(properties, options);
+
+                    if (!options.title) {
+                        options.title = options.defaultText;
+                    }
+
+                    if (options.collapseAfterChange === 'false') {
+                        options.collapseAfterChange = false;
+                    }
+
                     this.$super(arguments);
                 },
 
@@ -106,7 +116,8 @@ define(
                             rawValue = rawValue[0];
                         }
                         if (rawValue && rawValue[this.textField]) {
-                            displayText = rawValue[this.textField];
+                            displayText = rawValue && rawValue[this.textField]
+                                ? rawValue[this.textField] : this.defaultText;
                         }
                     }
                     this.set('title', u.escape(displayText));
@@ -174,7 +185,7 @@ define(
          * @param {Object} e 事件对象
          */
         function addHandler(e) {
-            if (this.collapseAfterChange) {
+            if (this.collapseAfterChange && this.hasState('expanded')) {
                 this.toggleContent();
             }
         }
